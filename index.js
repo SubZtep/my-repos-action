bdf63f91c711c63979154ea942661acc90dac14c
Andras Serfozo <subztep@gmail.com>
2021-01-24 20:04:58 +0000

    Test :gub:

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { config } = require("dotenv")
+const { graphql } = require("@octokit/graphql")
+const { format, formatDistance } = require("date-fns")
+config()
+
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
+// user(login: "${process.argv.pop()}") {
+
+const {
+  user: {
+    repositories: { nodes },
+  },
+} = await graphql(
+  `
+    {
+      user(login: "SubZtep") {
+        repositories(orderBy: { field: UPDATED_AT, direction: DESC }, first: 100, privacy: PUBLIC) {
+          nodes {
+            id
+            url
+            createdAt
+            description
+            homepageUrl
+            isArchived
+            isTemplate
+            isUserConfigurationRepository
+            name
+            updatedAt
+          }
+        }
+      }
+    }
+  `,
+  {
+    headers: {
+      authorization: `token ${process.env.GH_ACCESS_TOKEN}`,
+    },
+  }
+)
+
+const heads = ["Project", "Description", "Web", "Archived", "Updated", "Since"]
+let markdown = `| ${heads.join(" | ")} |\n|${" - |".repeat(heads.length)}`
+
+nodes.forEach(repo => {
+  const cols = [
+    `[${repo.name}](${repo.url})`,
+    repo.description,
+    repo.homepageUrl ? `[:link:](${repo.homepageUrl})` : "",
+    repo.isArchived ? ":heavy_check_mark:" : "",
+    capitalize(formatDistance(new Date(), new Date(repo.updatedAt))) + " ago",
+    format(new Date(repo.createdAt), "Y"),
+  ]
+  markdown += `| ${cols.join(" | ")} |`
+})
+markdown += "> :hourglass: " + new Date()
+
+console.log(markdown) 
 
 try {
   // `who-to-greet` input defined in action metadata file
