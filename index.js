004bde531f65952fdf1b46354d932238d543aec8
Andras Serfozo <subztep@gmail.com>
2021-01-27 11:05:27 +0000

    Fix

--- index.js
+++ index.js
@@ -5,56 +5,57 @@ const { graphql } = require("@octokit/graphql")
 const { format, formatDistance } = require("date-fns")
 config()
 
-const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
-// user(login: "${process.argv.pop()}") {
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
-const {
-  user: {
-    repositories: { nodes },
-  },
-} = await graphql(
-  `
-    {
-      user(login: "SubZtep") {
-        repositories(orderBy: { field: UPDATED_AT, direction: DESC }, first: 100, privacy: PUBLIC) {
-          nodes {
-            id
-            url
-            createdAt
-            description
-            homepageUrl
-            isArchived
-            isTemplate
-            isUserConfigurationRepository
-            name
-            updatedAt
+(async function () {
+  const {
+    user: {
+      repositories: { nodes },
+    },
+  } = await graphql(
+    `
+      {
+        user(login: "SubZtep") {
+          repositories(orderBy: { field: UPDATED_AT, direction: DESC }, first: 100, privacy: PUBLIC) {
+            nodes {
+              id
+              url
+              createdAt
+              description
+              homepageUrl
+              isArchived
+              isTemplate
+              isUserConfigurationRepository
+              name
+              updatedAt
+            }
           }
         }
       }
+    `,
+    {
+      headers: {
+        authorization: `token ${process.env.GH_ACCESS_TOKEN}`,
+      },
     }
-  `,
-  {
-    headers: {
-      authorization: `token ${process.env.GH_ACCESS_TOKEN}`,
-    },
-  }
-)
+  )
 
-const heads = ["Project", "Description", "Web", "Archived", "Updated", "Since"]
-let markdown = `| ${heads.join(" | ")} |\n|${" - |".repeat(heads.length)}`
+  const heads = ["Project", "Description", "Web", "Archived", "Updated", "Since"]
+  let markdown = `| ${heads.join(" | ")} |\n|${" - |".repeat(heads.length)}`
 
-nodes.forEach(repo => {
-  const cols = [
-    `[${repo.name}](${repo.url})`,
-    repo.description,
-    repo.homepageUrl ? `[:link:](${repo.homepageUrl})` : "",
-    repo.isArchived ? ":heavy_check_mark:" : "",
-    capitalize(formatDistance(new Date(), new Date(repo.updatedAt))) + " ago",
-    format(new Date(repo.createdAt), "Y"),
-  ]
-  markdown += `| ${cols.join(" | ")} |`
-})
-markdown += "> :hourglass: " + new Date()
+  nodes.forEach(repo => {
+    const cols = [
+      `[${repo.name}](${repo.url})`,
+      repo.description,
+      repo.homepageUrl ? `[:link:](${repo.homepageUrl})` : "",
+      repo.isArchived ? ":heavy_check_mark:" : "",
+      capitalize(formatDistance(new Date(), new Date(repo.updatedAt))) + " ago",
+      format(new Date(repo.createdAt), "Y"),
+    ]
+    markdown += `| ${cols.join(" | ")} |`
+  })
+  markdown += "> :hourglass: " + new Date()
 
-core.setOutput("table", markdown);
-console.log("XXX", markdown) 
+  core.setOutput("table", markdown);
+  console.log("XXX", markdown)
+})()
